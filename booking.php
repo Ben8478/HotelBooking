0e89be91e1d4dd2d50a04f991a1b8366a27b7dc4
Omarion23 <alexander.katlego23@gmail.com>
2020-03-18 10:58:16 +0200

    edit background image to booking page and display message with user name

--- booking.php
+++ booking.php
@@ -1,18 +1,23 @@
 
+<?php session_start() ?>
 <!doctype html>
+
 <html lang="en">
+
   <head>
-    <title>Title</title>
-   <link rel= "stylesheet" type="text/css" href ="css/style.css">
+    <title>Booking</title>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
-  <body>
+  <body id= "booking_image">
+
+    <h1>Thank you booking <?php  echo $_SESSION["users"] ?></h1>
   <?php 
-//php booking code
+
 
 ?>   
     <!-- Optional JavaScript -->
--- comparison.php
+++ comparison.php
@@ -16,7 +16,9 @@ if (isset($_POST)) { // if isset looks to see if the $_POST variable exists
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-
+  <form action ="booking.php">
+<button type="submit" >Booking</button>
+</form>
 
   <h1>Welcome to our booking page <?php echo $name ?></h1>
       <?php 
