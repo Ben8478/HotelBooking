559147e4551246f2be27282903a3d482f99aea5a
ReeceB4 <bucktonreece@gmail.com>
2020-03-19 15:52:41 +0200

    added logo,changed navbar color, created footer

--- comparison.php
+++ comparison.php
@@ -13,8 +13,8 @@
 
   <body>
 
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #0cea9e;">
-  <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><img src="images/logo.png" alt="logo" class="logo-pic"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -22,16 +22,18 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#" ><p class="nav-text">Home</p> <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#"><p class="nav-text">Booking</p></a>
       </li>
+
+     
       
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-success my-2 my-sm-0 ouline-color" type="submit">Search</button>
     </form>
   </div>
 </nav>
@@ -54,7 +56,7 @@
           
           <fieldset>
           <legend>Destination Details:</legend><br>
-          <img src="" alt=""><label for="hotel1">Hotel 1:</label>
+          <img src="images/img-book2.png" alt="" class="book-img"><label for="hotel1">Hotel 1:</label>
               <select id="allhotels" name="allhotels">
                   <option value=""></option>
                   <option value=""></option>
@@ -62,7 +64,7 @@
                   <option value=""></option>
               </select>
           
-          <img src="" alt=""><label for="hotel2">Hotel 2:</label>
+          <img src="images/img-book2.png" alt="" class="book-img"><label for="hotel2">Hotel 2:</label>
               <select id="allhotels" name="allhotels">
                   <option value=""></option>
                   <option value=""></option>
@@ -70,10 +72,10 @@
                   <option value=""></option>
               </select>
           <br><br>
-          <label for="checkin">Check in date:</label><br>
+          <img src="images/img-book.png" alt="" class="book-img"><label for="checkin">Check in date:</label><br>
           <input type="date" id="checkin" name="indate" min="2020-01-01" max="2023-12-31" required>
           <br><br>
-          <label for="checkout">Check out date:</label><br>
+          <img src="images/img-book.png" alt="" class="book-img"><label for="checkout">Check out date:</label><br>
           <input type="date" id="checkout" name="outdate" min="2020-01-01" max="2023-12-31" required>
           <br>
           <br>
@@ -152,7 +154,12 @@
     </div>
   </div>
   <br>
-    
+  <br>
+  <footer >
+    <p>Posted by: Reece, Ben, Alex</p>
+    <p>Copyright@KingLionHotels</p>
+  </footer>  
+
 
 
 
